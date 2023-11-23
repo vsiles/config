5a12e4bc97ccb49d0cafbf592ae795c518595d04
Vincent Siles <vsiles@meta.com>
2023-11-23 21:49:11 +0100

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -43,46 +43,56 @@ config.scrollback_lines = 5000
 -- key bindings
 config.leader = { key = 'b', mods = 'CTRL' }
 
+-- Show which key table is active in the status area
+wezterm.on('update-right-status', function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = 'TABLE: ' .. name
+  end
+  window:set_right_status(name or '')
+end)
+
 config.keys = {
-    -- TODO learn what that is
-    -- { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-    { key = 'x', mods = 'LEADER',          action = wezterm.action.CloseCurrentPane { confirm = true }},
-    { key = 's', mods = 'LEADER',          action = wezterm.action.ShowLauncher },
-    { key = 'P', mods = 'LEADER|SHIFT',          action = wezterm.action.ActivateCommandPalette },
+    { key = 'x', mods = 'LEADER',          action = act.CloseCurrentPane { confirm = true }},
+    { key = 's', mods = 'LEADER',          action = act.ShowLauncher },
+    { key = 'P', mods = 'LEADER|SHIFT',    action = act.ActivateCommandPalette },
     -- TODO create a local detachable domain
-    { key = "d", mods = "LEADER",          action = wezterm.action.DetachDomain "CurrentPaneDomain"}, 
-    -- { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-    { key = 'x', mods = 'LEADER|CTRL',     action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }},
-    { key = 'v', mods = 'LEADER|CTRL',     action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }},
-    { key = 'q', mods = 'LEADER',          action = wezterm.action.PaneSelect },
-    { key = "n", mods = "LEADER",          action = wezterm.action.ActivateTabRelative(1) },
-    { key = "p", mods = "LEADER",          action = wezterm.action.ActivateTabRelative(-1) },
-    { key = "z", mods = "LEADER",          action = wezterm.action.TogglePaneZoomState },
-    { key = "c", mods = "LEADER",          action = wezterm.action.SpawnTab "CurrentPaneDomain"},
-    { key = "h", mods = "LEADER",          action = wezterm.action.ActivatePaneDirection "Left"},
-    { key = "j", mods = "LEADER",          action = wezterm.action.ActivatePaneDirection "Down"},
-    { key = "k", mods = "LEADER",          action = wezterm.action.ActivatePaneDirection "Up"},
-    { key = "l", mods = "LEADER",          action = wezterm.action.ActivatePaneDirection "Right"},
-    { key = "LeftArrow", mods = "LEADER",  action = wezterm.action.ActivatePaneDirection "Left"},
-    { key = "DownArrow", mods = "LEADER",  action = wezterm.action.ActivatePaneDirection "Down"},
-    { key = "UpArrow",   mods = "LEADER",  action = wezterm.action.ActivatePaneDirection "Up"},
-    { key = "RightArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right"},
-    { key = "H", mods = "LEADER|SHIFT",    action = wezterm.action.AdjustPaneSize {"Left", 5}},
-    { key = "J", mods = "LEADER|SHIFT",    action = wezterm.action.AdjustPaneSize {"Down", 5}},
-    { key = "K", mods = "LEADER|SHIFT",    action = wezterm.action.AdjustPaneSize {"Up", 5}},
-    { key = "L", mods = "LEADER|SHIFT",    action = wezterm.action.AdjustPaneSize {"Right", 5}},
-    -- { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-    -- { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-    -- { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-    -- { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-    -- { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-    -- { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-    -- { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-    -- { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-    -- { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+    { key = "d", mods = "LEADER",          action = act.DetachDomain "CurrentPaneDomain"}, 
+    -- { key = "&", mods = "LEADER|SHIFT", action=act{CloseCurrentTab={confirm=true}}},
+    { key = 'x', mods = 'LEADER|CTRL',     action = act.SplitHorizontal { domain = "CurrentPaneDomain" }},
+    { key = 'v', mods = 'LEADER|CTRL',     action = act.SplitVertical { domain = "CurrentPaneDomain" }},
+    { key = 'q', mods = 'LEADER',          action = act.PaneSelect },
+    { key = "n", mods = "LEADER",          action = act.ActivateTabRelative(1) },
+    { key = "p", mods = "LEADER",          action = act.ActivateTabRelative(-1) },
+    { key = "z", mods = "LEADER",          action = act.TogglePaneZoomState },
+    { key = "c", mods = "LEADER",          action = act.SpawnTab "CurrentPaneDomain"},
+    -- Leader hjkl (or arrows) is a oneshot move to the relevant direction.
+    { key = "h", mods = "LEADER",          action = act.ActivatePaneDirection "Left"},
+    { key = "j", mods = "LEADER",          action = act.ActivatePaneDirection "Down"},
+    { key = "k", mods = "LEADER",          action = act.ActivatePaneDirection "Up"},
+    { key = "l", mods = "LEADER",          action = act.ActivatePaneDirection "Right"},
+    { key = "LeftArrow", mods = "LEADER",  action = act.ActivatePaneDirection "Left"},
+    { key = "DownArrow", mods = "LEADER",  action = act.ActivatePaneDirection "Down"},
+    { key = "UpArrow",   mods = "LEADER",  action = act.ActivatePaneDirection "Up"},
+    { key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection "Right"},
+    -- Leader a enters a move of moving between panes, that has to be
+    -- cancelled explicitly
+    { key = 'a', mods = 'LEADER',
+      -- action = act.ActivateKeyTable { name = 'activate_pane', timeout_milliseconds = 1000, },
+      action = act.ActivateKeyTable { name = 'activate_pane', one_shot = false, },
+    },
+    -- TODO same question for resizing panes
+    { key = "H", mods = "LEADER|SHIFT",    action = act.AdjustPaneSize {"Left", 5}},
+    { key = "J", mods = "LEADER|SHIFT",    action = act.AdjustPaneSize {"Down", 5}},
+    { key = "K", mods = "LEADER|SHIFT",    action = act.AdjustPaneSize {"Up", 5}},
+    { key = "L", mods = "LEADER|SHIFT",    action = act.AdjustPaneSize {"Right", 5}},
+    { key = 'r', mods = 'LEADER',
+      action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false, },
+    },
+    { key = "?", mods = "LEADER|SHIFT",    action = act.Search "CurrentSelectionOrEmptyString" },
     -- rename tab with prompt
     { key = ",", mods = "LEADER",
-      action = wezterm.action.PromptInputLine {
+      action = act.PromptInputLine {
           description = "Enter new name for tab",
           action = wezterm.action_callback(function(window, pane, line)
               -- https://github.com/wez/wezterm/issues/522
@@ -90,7 +100,55 @@ config.keys = {
           end),
       }
     },
-    { key = "[", mods = "LEADER",          action = wezterm.action.ActivateCopyMode },
+    { key = "[", mods = "LEADER",          action = act.ActivateCopyMode },
+}
+
+config.key_tables = {
+  -- Defines the keys that are active in our activate-pane mode.
+  -- 'activate_pane' here corresponds to the name="activate_pane" in
+  -- the key assignments above.
+  activate_pane = {
+    { key = 'LeftArrow', action = act.ActivatePaneDirection 'Left' },
+    { key = 'h', action = act.ActivatePaneDirection 'Left' },
+
+    { key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
+    { key = 'l', action = act.ActivatePaneDirection 'Right' },
+
+    { key = 'UpArrow', action = act.ActivatePaneDirection 'Up' },
+    { key = 'k', action = act.ActivatePaneDirection 'Up' },
+
+    { key = 'DownArrow', action = act.ActivatePaneDirection 'Down' },
+    { key = 'j', action = act.ActivatePaneDirection 'Down' },
+
+    -- Exit options
+    { key = 'Return', action = 'PopKeyTable' },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'q', action = 'PopKeyTable' },
+  },
+  -- Defines the keys that are active in our resize-pane mode.
+  -- Since we're likely to want to make multiple adjustments,
+  -- we made the activation one_shot=false. We therefore need
+  -- to define a key assignment for getting out of this mode.
+  -- 'resize_pane' here corresponds to the name="resize_pane" in
+  -- the key assignments above.
+  resize_pane = {
+    { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 5 } },
+    { key = 'h', action = act.AdjustPaneSize { 'Left', 5 } },
+
+    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 5 } },
+    { key = 'l', action = act.AdjustPaneSize { 'Right', 5 } },
+
+    { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 5 } },
+    { key = 'k', action = act.AdjustPaneSize { 'Up', 5 } },
+
+    { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 5 } },
+    { key = 'j', action = act.AdjustPaneSize { 'Down', 5 } },
+
+    -- Cancel the mode by pressing escape
+    { key = 'Return', action = 'PopKeyTable' },
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'q', action = 'PopKeyTable' },
+  },
 }
 
 -- config I don't understand yet :)
