5c7d8055e1953132671386d04756223048f48121
Vincent Siles <vincent.siles@ens-lyon.org>
2023-11-25 12:38:55 +0100

    no opam on windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -403,41 +403,42 @@ autocmd('FileType', {
 	end,
 })
 
--- ocaml / merlin
-local opam_share_dir = vim.fn.system { 'opam', 'var', 'share' }
-opam_share_dir = opam_share_dir:gsub("[\r\n]*$", "")
--- print(opam_share_dir)
+if not (vim.fn.has('win32') or vim.fn.has('win64')) then
+    -- ocaml / merlin
+    local opam_share_dir = vim.fn.system { 'opam', 'var', 'share' }
+    opam_share_dir = opam_share_dir:gsub("[\r\n]*$", "")
+    -- print(opam_share_dir)
 
-local ocp_indent_path = opam_share_dir .. '/ocp-indent/vim'
-local ocp_index_path = opam_share_dir .. '/ocp-index/vim'
-local ocaml_merlin_path = opam_share_dir .. '/merlin/vim'
+    local ocp_indent_path = opam_share_dir .. '/ocp-indent/vim'
+    local ocp_index_path = opam_share_dir .. '/ocp-index/vim'
+    local ocaml_merlin_path = opam_share_dir .. '/merlin/vim'
 
-if doesDirectoryExist(ocp_indent_path) then
-  vim.cmd('set runtimepath+=' .. ocp_indent_path)
+    if doesDirectoryExist(ocp_indent_path) then
+        vim.cmd('set runtimepath+=' .. ocp_indent_path)
+    end
+
+    if doesDirectoryExist(ocp_index_path) then
+        vim.cmd('set runtimepath+=' .. ocp_index_path)
+    end
+
+    if doesDirectoryExist(ocaml_merlin_path) then
+        vim.cmd('set runtimepath+=' .. ocaml_merlin_path)
+    end
+
+    -- Using Fred's ocp-indent
+    --   source "/home/vsiles/.vim/bundle/ocp-indent-vim/indent/ocaml.vim"
+
+    -- " Target is displayed in a new tab
+    -- let g:merlin_split_method = "tab"
+    vim.g.merlin_split_method = "tab"
+    -- let g:merlin_locate_preference = "ml"
+    vim.g.merlin_locate_preference = "ml"
 end
-
-if doesDirectoryExist(ocp_index_path) then
-  vim.cmd('set runtimepath+=' .. ocp_index_path)
-end
-
-if doesDirectoryExist(ocaml_merlin_path) then
-  vim.cmd('set runtimepath+=' .. ocaml_merlin_path)
-end
-
--- Using Fred's ocp-indent
---   source "/home/vsiles/.vim/bundle/ocp-indent-vim/indent/ocaml.vim"
-
--- " Target is displayed in a new tab
--- let g:merlin_split_method = "tab"
-vim.g.merlin_split_method = "tab"
--- let g:merlin_locate_preference = "ml"
-vim.g.merlin_locate_preference = "ml"
 
 
 -- ale
 vim.g.ale_use_neovim_diagnostics_api = 1
 vim.g.ale_linters = { python = {} , rust = {'analyzer'} }
--- vim.g.ale_linters = { python = {} }
 vim.g.ale_rust_analyzer_executable = '/data/users/vsiles/my-rust/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rust-analyzer'
 
 -- rust
